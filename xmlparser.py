2f30059934d9a0d9c5026a74c1d129648ae45b90
tinaswang <tswang@caltech.edu>
2016-06-08 15:10:13 -0400

    Added array-viewing functionality
    The arrays in the original data are now preserved.

--- xmlparser.py
+++ xmlparser.py
@@ -6,34 +6,35 @@ class Converter:
         parser = etree.XMLParser(remove_comments=True)
         xmldoc = etree.parse(filename, parser = parser)
         root = xmldoc.getroot()
-        result = [xmldoc, root]
         print("{\n" +  "\"" + root.tag + "\"" + ": {")
-        return root
+        return xmldoc
 
     def convert(self, filename, root):
 
-        for detector in root.iter("Detector"):
-            if detector.attrib =="type":
-                dimensions = "INT32[192,256]".replace("[", " ").replace(",", " ").replace("]", " ").split()
-                if len(dimensions) == 3 and dimensions[0] == "INT32":
-                    dimensions.remove("INT32")
-                    dimensions =[int(i) for i in mylist]
-                    datalist = self.arraysplit(detector.text, dimensions)
-                    print(datalist)
 
         for child in root:
             print("\"" + child.tag+ "\"" + ": {")
-
             for attribute in child.attrib:
                     data = child.get(attribute)
                     print("\t\"-" + attribute+  "\": \"" + data+ "\",")
-
-            if child.text is not None:
-                print("\t\"#text\": \"" + child.text+ "\"\n\t},")
+            if child.text:
+                if child.tag =="Detector":
+                    if any("type" in s for s in child.attrib):
+                        dimensions = "INT32[192,256]".replace("[", " ").replace(",", " ").replace("]", " ").split()
+                        if len(dimensions) == 3 and dimensions[0] == "INT32":
+                            dimensions.remove("INT32")
+                            dimensions =[int(i) for i in dimensions]
+                            data = self.arraysplit(child.text.split(), dimensions)
+                            self.prettyprint(data)
+                else:
+                    print("\t\"#text\": \"" + child.text+ "\"\n\t},")
             [self.convert(filename, child) for children in child]
-        print("},")
+    #        if (len(root[2])):
+        print("}")
+    #        else:
+    #            print("},")
 
-    def arraysplit(data, dimensions):
+    def arraysplit(self, data, dimensions):
         data = [int(i) for i in data]
         rows = dimensions[0]
         cols = dimensions[1]
@@ -41,11 +42,12 @@ class Converter:
         return datalist
 
     def prettyprint(self, data):
+        print("\t-data: \"")
         for row in data:
             for value in row:
                 print (value, end = ' ')
-            print("")
-
+            print(" ")
+        print("\"\n}")
 
 converter = Converter()
-converter.convert("dataexample.xml",converter.setup("dataexample.xml"))
+converter.convert("dataexample.xml",converter.setup("dataexample.xml").getroot())
